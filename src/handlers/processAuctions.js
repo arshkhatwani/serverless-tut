5a833d93d17a6a0af774e0bc2ffea5ff3cc348f5
arshkhatwani <arsh.khatwani@gmail.com>
2024-02-24 16:21:04 +0530

    Adds function to close auctions

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,10 +1,24 @@
+import { closeAuction } from "../lib/closeAuction";
 import { getEndedAuctions } from "../lib/getEndedAuctions";
+import createError from "http-errors";
 
 async function processAuctions(event, context) {
     console.log("Processing auctions");
+    try {
+        const auctionsToClose = await getEndedAuctions();
+        console.log("Auctions to close:", auctionsToClose);
 
-    const auctionsToClose = await getEndedAuctions();
-    console.log(auctionsToClose);
+        const closePromises = auctionsToClose.map((auction) =>
+            closeAuction(auction)
+        );
+        await Promise.all(closePromises);
+        console.log(closePromises.length, "auctions closed successfully");
+
+        return { closed: closePromises.length };
+    } catch (error) {
+        console.error(error);
+        throw new createError.InternalServerError(error);
+    }
 }
 
 export const handler = processAuctions;
